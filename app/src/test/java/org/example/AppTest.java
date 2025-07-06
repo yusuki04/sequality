5516436c51a1780b2691a922d74d0c3d78fe947f
yusuki04 <yusuriku@icloud.com>
2025-07-06 21:03:03 +0900

    XXXX

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -11,4 +11,11 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+    @Test
+public void testAppReturnHelloWorld() {
+    App classUnderTest = new App();
+    assertEquals("Hello World!", classUnderTest.getGreeting());
 }
+
+}
+
