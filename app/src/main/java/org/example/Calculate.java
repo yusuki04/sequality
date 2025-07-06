06168d8be8a96dadc53918ac22d293f516d31977
yusuki04 <yusuriku@icloud.com>
2025-07-06 23:38:51 +0900

    test for Calculate.java

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -1,8 +1,39 @@
 package org.example;
 
 public class Calculate {
-    public int sum(int x, int y)
-{
-    return x + y;
-}
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public double average(int a, int b) {
+        return (a + b) / 2.0;
+    }
+
+    public int sumBetween(int from, int to) {
+        int sum = 0;
+        for (int i = from; i <= to; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public double averageBetween(int from, int to) {
+        return sumBetween(from, to) / (double)(to - from + 1);
+    }
+
+    public int sumOddBetween(int from, int to) {
+        int sum = 0;
+        for (int i = from; i <= to; i++) {
+            if (i % 2 == 1) sum += i;
+        }
+        return sum;
+    }
+
+    public int sumEvenBetween(int from, int to) {
+        int sum = 0;
+        for (int i = from; i <= to; i++) {
+            if (i % 2 == 0) sum += i;
+        }
+        return sum;
+    }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -4,10 +4,40 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculateTest {
+
     @Test
-    public void testSum() {
-        Calculate calculate = new Calculate();
-        int expected = 5;
-        assertEquals(expected, calculate.sum(2, 3));
+    public void testSum_TwoAndThree_ReturnsFive() {
+        Calculate calc = new Calculate();
+        assertEquals(5, calc.sum(2, 3));
+    }
+
+    @Test
+    public void testAverage_TwoAndThree_ReturnsTwoPointFive() {
+        Calculate calc = new Calculate();
+        assertEquals(2.5, calc.average(2, 3), 0.001);
+    }
+
+    @Test
+    public void testSumBetween_OneToTen_Returns55() {
+        Calculate calc = new Calculate();
+        assertEquals(55, calc.sumBetween(1, 10));
+    }
+
+    @Test
+    public void testAverageBetween_OneToTen_ReturnsFivePointFive() {
+        Calculate calc = new Calculate();
+        assertEquals(5.5, calc.averageBetween(1, 10), 0.001);
+    }
+
+    @Test
+    public void testSumOddBetween_OneToTen_Returns25() {
+        Calculate calc = new Calculate();
+        assertEquals(25, calc.sumOddBetween(1, 10));
+    }
+
+    @Test
+    public void testSumEvenBetween_OneToTen_Returns30() {
+        Calculate calc = new Calculate();
+        assertEquals(30, calc.sumEvenBetween(1, 10));
     }
 }
