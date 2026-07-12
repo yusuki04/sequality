cb18b20999ad197747bdc232929f63f4b046950c
yusuki04 <yusuriku@icloud.com>
2026-07-12 17:02:49 +0900

    XXXX

---
+++ app/src/test/java/org/example/CalculateTest.java
@@ -0,0 +1,12 @@
+package org.example;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class CalculateTest {
+@Test
+public void testSum() {
+Calculate calculate = new Calculate();
+int expected = 5;
+assertEquals(expected, calculate.sum(2, 3));
+}
+
+}
